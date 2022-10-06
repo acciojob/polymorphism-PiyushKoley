228a8c4ea16ef71a53992f52617efc230e1b7f7b
piyushkoley <piyushkoley@gmail.com>
2022-10-07 01:45:18 +0530

    made changes as per Read Me

--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -1,7 +1,5 @@
 package com.driver;
 
-import java.sql.SQLOutput;
-
 public class Main {
     public static void main(String [] args) {
         Product p = new Product();
@@ -10,19 +8,21 @@ public class Main {
         p.product(2.0,3.0);
 
     }
-    static class Product {
 
-        public int product(int x, int y) {
+}
 
-            return 1;
-        }
+class Product {
 
-        public int product(int x, int y, int z) {
-            return 2;
-        }
+    public int product(int x, int y) {
 
-        public double product(double x, double y) {
-            return 3;
-        }
+        return 1;
+    }
+
+    public int product(int x, int y, int z) {
+        return 2;
+    }
+
+    public double product(double x, double y) {
+        return 3;
     }
 }
