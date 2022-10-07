8d8a280b4268507c30cef78bfff5591160ab48af
piyushkoley <piyushkoley@gmail.com>
2022-10-07 21:46:50 +0530

    make required changes

--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -3,26 +3,28 @@ package com.driver;
 public class Main {
     public static void main(String [] args) {
         Product p = new Product();
-        p.product(1,2);
-        p.product(12,13,4);
-        p.product(2.0,3.0);
+        int first = p.product(1,2);
+        int second = p.product(12,13,4);
+        double third = p.product(2.0,3.0);
 
+        System.out.println(first +" "+ second +" "+ third);
     }
 
+    public static class Product {
+
+        public int product(int x, int y) {
+
+            return x*y;
+        }
+
+        public int product(int x, int y, int z) {
+
+            return x*y*z;
+        }
+
+        public double product(double x, double y) {
+            return x*y;
+        }
+    }
 }
 
-class Product {
-
-    public int product(int x, int y) {
-
-        return 1;
-    }
-
-    public int product(int x, int y, int z) {
-        return 2;
-    }
-
-    public double product(double x, double y) {
-        return 3;
-    }
-}
